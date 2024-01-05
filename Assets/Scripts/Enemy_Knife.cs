94fd053a6a84914d25d8670c3fd7e37065e9d835
YIGITSOMEN\yyeg2 <yyeg214@gmail.com>
2024-01-06 02:50:12 +0300

    Knife animation bug fixes

--- Assets/Scripts/Enemy_Knife.cs
+++ Assets/Scripts/Enemy_Knife.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class Enemy_Knife : MonoBehaviour
 {
+   [SerializeField] private float MovementDistance;
    [SerializeField] private float damage;
+   [SerializeField] private float speed;
 
+   private bool movingLeft;
+   private float LeftEdge;
+   private float RightEdge;
+   
+
+   private void Awake()
+   {
+      LeftEdge = transform.position.x - MovementDistance;
+      RightEdge = transform.position.x + MovementDistance;
+   }
+
+   private void Update()
+   {
+      if (movingLeft)
+      {
+         if (transform.position.x > LeftEdge)
+         {
+               transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
+         else
+            movingLeft = false;
+      }
+      else
+      {
+         if (transform.position.x < RightEdge)
+         {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
+         else
+            movingLeft = false;
+      }
+   }
    private void OnTriggerEnter2D(Collider2D collision)
    {
       if (collision.tag == "Player")

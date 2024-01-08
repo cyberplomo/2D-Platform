0f01f5fa7aefde4e55d75f0188f5bc845ae84ed7
YIGITSOMEN\yyeg2 <yyeg214@gmail.com>
2024-01-08 21:14:48 +0300

    Some different traps are added on project.

---
+++ Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ArrowTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] arrow;
+    private float cooldownTimer;
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        arrow[FindArrow()].transform.position = firePoint.position;
+        arrow[FindArrow()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+    private int FindArrow()
+    {
+        for (int i = 0; i < arrow.Length; i++)
+        {
+            if (!arrow[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (cooldownTimer >= attackCooldown)
+            Attack();
+    }
+    
+}

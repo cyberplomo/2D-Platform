cb1d10a5a7d0ddc34a19161fc20d61106e19a2b4
YIGITSOMEN\yyeg2 <yyeg214@gmail.com>
2024-01-15 22:44:09 +0300

    Fix Some Respawn problems.

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -9,6 +9,18 @@ public class PlayerRespawn : MonoBehaviour
   [SerializeField] private AudioClip checkpointSound;
   private Transform currentCheckpoint;
   private Health playerHealth;
+  private Vector3 respawnPoint;
+  public GameObject fallDetector;
+
+  private void Start()
+  {
+    respawnPoint = transform.position;
+  }
+
+  private void Update()
+  {
+    fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+  }
 
   private void Awake()
   {
@@ -37,6 +49,10 @@ public class PlayerRespawn : MonoBehaviour
     else if (collision.transform.tag == "PreviousLevel")
     {
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    else if (collision.transform.tag == "FallDetector")
+    {
+      transform.position = respawnPoint;
     }
   }
 }

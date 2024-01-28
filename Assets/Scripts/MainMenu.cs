08b79da72858bd34522805a98123c0943e0f2a03
YIGITSOMEN\yyeg2 <yyeg214@gmail.com>
2024-01-28 20:11:12 +0300

    some fixes.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+  
    public void PlayGame()
    {
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      Time.timeScale = 1;
    }
 
    public void QuitGame()
